8caf8d6b16681b1815af4ca97bb0c5ef79fbd81a
Mércio <mercio20a@gmail.com>
2024-11-27 17:54:55 -0300

    :bug: :white_check_mark: fixed bug in the form, successfull login, and redirecting to route myprofile

--- linktree-clone/src/app/app.routes.ts
+++ linktree-clone/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { MyprofileComponent } from './pages/myprofile/myprofile.component';
 
 export const routes: Routes = [
 {
     component: LoginComponent, path:'',
+    
+},
+{
+    component: MyprofileComponent, path:'myprofile'
 }
     
 ];
--- linktree-clone/src/app/pages/login/login.component.ts
+++ linktree-clone/src/app/pages/login/login.component.ts
@@ -2,25 +2,26 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-userName=new FormControl('');
+userName=new FormControl<string | null>('');
 password=new FormControl('');
+spanAlerta:boolean=false;
 
 constructor(private router:Router){}
-  verificarLogin(userName: string,password: string){
+  verificarLogin(userName: string | null,password: string | null){
     if(userName == 'mercio' && password== '1234' ){
+      
    this.router.navigate(['/myprofile']);
   }else{
-    document.addEventListener('DOMContentLoaded',()=>{
-      // document.getElementById('alert').style.display='block';
-    })
+   this.spanAlerta=true
     
   }
 }
