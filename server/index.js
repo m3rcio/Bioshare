6164ea48f37dfc4f4a8c830f2a0d922c99f86cfb
Mércio <mercio20a@gmail.com>
2024-12-05 21:54:59 -0300

    ...

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express= require('express');
 const app= express();
 const mongoose= require('mongoose');
 const port=3000
-// mongoose.connect('mongodb://localhost/users',{useNewUrlParser: true});
-// const db=mongoose.connection;
+mongoose.connect('mongodb://localhost/users');
+const db=mongoose.connection;
 
 // db.on('error',(error)=>console.error(error))
 // db.once('open',()=>console.log('connected to database'))
