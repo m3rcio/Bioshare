279ab645679a42cb201d609bf6a5acaba6de1c10
Mércio <mercio20a@gmail.com>
2024-12-07 23:16:25 -0300

    ...

--- server/demo_create_mongo_db.js
+++ server/demo_create_mongo_db.js
@@ -1,4 +1,5 @@
 let MongoClient= require('mongodb').MongoClient;
+
 const url='mongodb://127.0.0.1:27017/bioshare';
 
 MongoClient.connect(url, function(err, db) {
--- server/index.js
+++ server/index.js
@@ -2,17 +2,24 @@ const express= require('express');
 const app= express();
 const mongoose= require('mongoose');
 const port=3000
-mongoose.connect('mongodb://localhost/users');
-const db=mongoose.connection;
+// mongoose.connect('mongodb://127.0.0.1:27017/users');
+// const db=mongoose.connection;
 
 // db.on('error',(error)=>console.error(error))
 // db.once('open',()=>console.log('connected to database'))
 
+// app.get('/myprofile',(req,res)=>{
+//     console.log('hello world!')
+// })
 
+// let MongoClient= require('mongodb').MongoClient;
 
+const url='mongodb://127.0.0.1:27017/bioshare';
 
-app.get('/myprofile',(req,res)=>{
-    console.log('hello world!')
-})
+mongoose.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  });
 
 app.listen(port, ()=>console.log('server started at '+port))
