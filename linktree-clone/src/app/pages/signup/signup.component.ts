fe7d953d121273a7668411ca50288542760ae9d6
Mércio <mercio20a@gmail.com>
2025-01-03 06:10:41 -0300

    mongodb update

--- linktree-clone/src/app/pages/signup/signup.component.ts
+++ linktree-clone/src/app/pages/signup/signup.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-
+  userName=new FormControl<string | null>('');
+  password=new FormControl('');
+  spanAlerta:boolean=false;
 }
