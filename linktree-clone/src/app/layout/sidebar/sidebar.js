e1c9b90def5fc077d77b5121570b7ee51d7f66bf
Mércio <mercio20a@gmail.com>
2025-01-31 11:08:15 -0300

    still fixing the issues with the myprofile ui

---
+++ linktree-clone/src/app/layout/sidebar/sidebar.js
@@ -0,0 +1,4 @@
+var buttonToggle=document.getElementById('open_btn');
+buttonToggle.addEventListener('click', function(){
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+})
