a98697cc828d9e8d2909f11b49afc2488c562d31
Mércio <mercio20a@gmail.com>
2025-02-23 15:14:33 -0300

    mudanças no backend socialLinks

--- linktree-clone/src/app/pages/myprofile/dashboard/dashboard.component.ts
+++ linktree-clone/src/app/pages/myprofile/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { NgIf } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterOutlet,RouterLink } from '@angular/router';
+import { socialLinks } from '../../../models/sociallinks.model';
 @Component({
   selector: 'app-dashboard',
   imports: [ReactiveFormsModule,CommonModule,RouterLink ],
@@ -14,6 +15,13 @@ import { RouterOutlet,RouterLink } from '@angular/router';
 export class DashboardComponent {
 
   @Input() sidebarOpen = false;
+
+  socialLink: socialLinks={
+    title:'',
+    url:'',
+    isActive:false,
+    icon:''
+  }
 
   criarSocialLink(){
     return 0;
