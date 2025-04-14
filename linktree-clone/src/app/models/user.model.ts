b63bab1176a0499dea59d2bcc625e3cd2d2a58e6
Mércio <mercio20a@gmail.com>
2025-04-14 14:04:42 +0100

    adicionada métodos no userService

--- linktree-clone/src/app/models/user.model.ts
+++ linktree-clone/src/app/models/user.model.ts
@@ -1,4 +1,4 @@
-export interface user
+export interface User
 {
     user_id:number,
     nome:string,
--- linktree-clone/src/app/services/user.service.ts
+++ linktree-clone/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {user} from "../models/user.model";
+import {User} from "../models/user.model";
 
 @Injectable({
     providedIn: 'root'
@@ -10,4 +10,34 @@ import {user} from "../models/user.model";
     private apiBaseUrl = 'http://localhost:3000/api';
   
     constructor(private http: HttpClient) { }
+
+        criarUser(User: User): Observable<User> {
+          const apiUrl = `${this.apiBaseUrl}/signup`;
+          return this.http.post<User>(apiUrl, { User});
+        }
+       
+        atualizarUser(User_id:number,Users: User): Observable<User> {
+          const apiUrl = `${this.apiBaseUrl}/social-links/${User_id}`;
+          return this.http.put<User>(apiUrl, Users);
+        }
+      
+        excluirUser(idUser: number): Observable<void> {
+          const apiUrl = `${this.apiBaseUrl}/social-links/${idUser}`;
+          return this.http.delete<void>(apiUrl);
+        }
+      
+        getUsers(): Observable<User[]> {
+          const apiUrl = `${this.apiBaseUrl}/social-links`;
+          return this.http.get<User[]>(apiUrl);
+        }
+      
+        getUsersById(idUsers: number): Observable<User> {
+          const apiUrl = `${this.apiBaseUrl}/social-links/${idUsers}`;
+          return this.http.get<User>(apiUrl);
+        }
+      
+        getUsersByUserId(user_id: number): Observable<User[]> {
+          const apiUrl = `${this.apiBaseUrl}/social-links/user/${user_id}`;
+          return this.http.get<User[]>(apiUrl);
+        }
   }
