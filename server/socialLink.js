ea83b83d57d8ff1de94c56083f52389278914b92
Mércio <mercio20a@gmail.com>
2025-08-27 14:50:52 +0100

    testes para o deploy

--- server/socialLink.js
+++ server/socialLink.js
@@ -1,14 +1,16 @@
-let mongoose=require("mongoose")
-const url='mongodb://127.0.0.1:27017/bioshare';
+let mongoose=require("mongoose");
+require('dotenv').config();
+const mongodb_connection_string= process.env.mongodb_connection_string;
+
 
 
 async function connectDB() {
     try {
-      await mongoose.connect(url, {
+      await mongoose.connect(mongodb_connection_string, {
       });
       console.log('Database connected successfully');
     } catch (error) {
-      console.error('Database connection error:', error);
+      console.error('Database connection error: ', error);
       process.exit(1);
     }
   }
